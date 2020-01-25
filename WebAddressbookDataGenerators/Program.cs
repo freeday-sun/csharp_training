29b85f73c88c4417154d1a6d917be547b178ad50
freeday-sun <gigdeer@yandex.ru>
2020-01-25 16:53:21 +0300

    add ConsoleApp DataGenerators in Solution

---
+++ WebAddressbookDataGenerators/Program.cs
@@ -0,0 +1,24 @@
+﻿using WebAddressbookTests;
+using System.IO;
+using System;
+
+namespace WebAddressbookDataGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < count; i++)
+            {
+                writer.WriteLine(String.Format("${0},{1},{2}",
+                    BaseTest.GenerateRandomString(25),
+                    BaseTest.GenerateRandomString(25),
+                    BaseTest.GenerateRandomString(25)
+                    ));
+            }
+            writer.Close();
+        }
+    }
+}
--- WebAddressbookTests/tests/GroupTests/GroupCreationTests.cs
+++ WebAddressbookTests/tests/GroupTests/GroupCreationTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WebAddressbookTests.tests.GroupTests
 {
@@ -20,7 +21,23 @@ namespace WebAddressbookTests.tests.GroupTests
             return groups;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<GroupData> GetGroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"D:\Courses\DotNet\csharp_training\WebAddressbookTests\data\groups.csv");
+            foreach (string line in lines)
+            {
+                string[] parts =  line.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1], Footer = parts[2]
+                });
+
+            }
+            return groups;
+        }
+
+        [Test, TestCaseSource("GetGroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupsList();
